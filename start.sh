7fe5f7f0297915bff240831d7bfd21f68d5db771
奥尘 <aiello.cxl@alibaba-inc.com>
2024-01-02 22:49:46 +0800

    fix: 使用 conda

--- start.sh
+++ start.sh
@@ -7,6 +7,8 @@ CONDA_ENV_PYTHON_VERSION=3.11
 
 ############## 逻辑代码，不用修改 ##############
 
+set -e
+
 # 处理 homebrew
 {
     if ! command -v brew &>/dev/null; then
@@ -15,41 +17,31 @@ CONDA_ENV_PYTHON_VERSION=3.11
     fi
 }
 
-# 处理 mamba
+# 处理 conda
 {
-    if command -v mamba &>/dev/null; then
-        eval "$(mamba shell hook --shell bash)"
-    elif command -v micromamba &>/dev/null; then
-        alias mamba='micromamba'
-        eval "$(mamba shell hook --shell bash)"
+    if command -v conda &>/dev/null; then
+        eval "$(conda shell.bash hook)"
     else
-        echo "mamba 未安装，正在安装 micromamba..."
-        brew install micromamba -y
-        micromamba shell init -s zsh -p ~/micromamba
-        alias mamba='micromamba'
+        echo "conda 未安装，正在安装 miniconda..."
+        brew install miniconda
     fi
 }
 
-# 处理 mamba 环境
+# 处理 conda 环境
 {
-    if mamba env list | grep -q $CONDA_ENV_NAME; then
-        mamba activate $CONDA_ENV_NAME
+    if conda env list | grep -q $CONDA_ENV_NAME; then
+        conda activate $CONDA_ENV_NAME
     else
         echo "$CONDA_ENV_NAME 环境不存在，创建中..."
-
-        mamba config append channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main
-        mamba config append channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free
-        mamba config append channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/conda-forge
-
-        mamba create --name $CONDA_ENV_NAME python=$CONDA_ENV_PYTHON_VERSION -y
-        mamba activate $CONDA_ENV_NAME
+        conda create --name $CONDA_ENV_NAME python=$CONDA_ENV_PYTHON_VERSION -y
+        conda activate $CONDA_ENV_NAME
     fi
 }
 
 # 处理依赖
 {
     # 以下就是先装一遍，因为如果有安装过，则不会重复安装
-    pip install torch torchvision torchaudio
+    pip install torch torchvision torchaudio 'httpx[socks]'
     pip install -r requirements.txt
 }
 
